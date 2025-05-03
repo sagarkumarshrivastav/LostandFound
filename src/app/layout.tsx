14c4c719349eff45734fbae8cba8d94d670d3248
Sagar Kumar Shrivastav <180.sagar@gmail.com>
2025-05-03 21:10:46 +0000

    make light mode in white theme.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,11 +26,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${inter.variable} font-sans antialiased flex flex-col min-h-screen bg-background text-foreground overflow-x-hidden`}> {/* Removed extra space before body, added overflow-x-hidden */}
+      <body className={`${inter.variable} font-sans antialiased flex flex-col min-h-screen bg-background text-foreground overflow-x-hidden`}>
          <ThemeProvider
           attribute="class"
-          defaultTheme="dark" // Set default theme to dark
-          enableSystem={false} // Disable system preference to enforce dark theme initially
+          defaultTheme="light" // Set default theme to light
+          enableSystem={false} // Disable system preference
           disableTransitionOnChange
         >
           <AuthProvider>
